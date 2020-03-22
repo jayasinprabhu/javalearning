94c6cf7061705437aae254ffcead719d36f5ed93
JAYASIN PRABHU C <ceo.jayasin@gmail.com>
2020-03-22 14:14:27 +0530

    Second session 22-03-2020

---
+++ DataTypes.java
@@ -0,0 +1,35 @@
+class DataTypes {
+    public static void main(String[] args) {
+        int firstNumber;
+        firstNumber = 10;
+        int secondNumber = 10;
+       // int secondNumber = 10;
+
+        int additionOfNumbers = firstNumber + secondNumber; // 20
+        int subractionOfNumbers = firstNumber - secondNumber; // 0
+        int multiplicationOfNumbers = firstNumber * secondNumber; // 100
+        int divisionOfNumbers = firstNumber / secondNumber; // 1
+        int moduloDivision = firstNumber % secondNumber; // 0 
+
+        /**
+         * + => Addition
+         * - => Subraction
+         * * => Multiplication
+         * / => Divistion
+         * % => Modulo Division
+         */
+
+        System.out.println("First Number " + firstNumber);
+        System.out.println("Second Number " + secondNumber);
+        System.out.println("Addition of Number " + additionOfNumbers);
+        System.out.println("subraction of Number " + subractionOfNumbers);
+        System.out.println("Multiplication of Number " + multiplicationOfNumbers);
+        System.out.println("Division of Number " + divisionOfNumbers);
+        System.out.println("Modulo of Number " + moduloDivision);
+    }
+
+}
+
+
+
+
